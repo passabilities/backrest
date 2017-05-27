f37fae7c6d8e2e0dfb03525bd9239d88a3a57ad5
Noah Passalacqua <noah.passa@gmail.com>
2017-05-26 23:08:38 -0400

    Set global __ENV__ variable from CLI.

--- bin/backrest-start.js
+++ bin/backrest-start.js
@@ -1,3 +1,7 @@
+// Set default environment to 'development'
+global.__ENV__ = process.env.NODE_ENV || 'development'
+global.__DEV__ = __ENV__ === 'development'
+
 const app = require('express')()
 app.use('/', require('../lib/routing/buildRoutes'))
 require(`${process.cwd()}/config/boot`)(app)
--- template/config/boot.js
+++ template/config/boot.js
@@ -3,10 +3,6 @@ const cookieParser = require('cookie-parser')
 
 const { logger } = require('backrest')
 
-// Set default environment to 'development'
-global.__ENV__ = process.env.NODE_ENV || 'development'
-global.__DEV__ = __ENV__ === 'development'
-
 // Use environment defined port or default to 3000
 const port = process.env.PORT || 3000
 
