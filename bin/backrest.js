a371a213a2c691e7f8a657d0b249802648fdd3d0
Noah Passalacqua <noah.passa@gmail.com>
2017-10-06 19:17:20 -0400

    Add cli version option.

--- bin/backrest.js
+++ bin/backrest.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
-let cli = require('commander')
+const cli = require('commander')
+const fs = require('fs')
+
+let { version } = JSON.parse(fs.readFileSync(`${__dirname}/../package.json`, 'utf8'))
 
 cli
+  .version(version)
   .command('init <name>', 'create blank Backrest template').alias('i')
   .command('start', 'start the server').alias('s')
   .command('stop', 'stop the server')
