b45928be5e91d937f0966aae941cbd7b0a16ae03
Noah Passalacqua <noah.passa@gmail.com>
2018-08-15 14:59:17 -0400

    Move app to seprate file and export it.

--- bin/start.js
+++ bin/start.js
@@ -1,27 +1,7 @@
-const _ = require('lodash')
-const glob = require('glob')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-
+const app = require('../app')
 const logger = require('../src/logger')
 
-const config = require(`${process.cwd()}/config/application`)
-
-// Set default environment to 'development'
-global.__ENV__ = process.env.NODE_ENV || 'development'
-global.__DEV__ = __ENV__ === 'development'
-
 const port = process.env.PORT || 7777
-
-const app = require('express')()
-
-// Evaluate initializer files before starting the server.
-_.each(glob.sync(`${process.cwd()}/config/initializers/*`), require)
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser(config.cookieSecret))
-app.use('/', require('../lib/routing/buildRoutes'))
 
 const server = app.listen(port, () => {
   logger.log([
